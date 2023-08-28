f4019eecbab24f87450e4eb550c38078e65cb2c7
Samuel NAIT <nait.samuel@gmail.com>
2023-08-28 21:37:13 +0200

    :sparkles: Header in distinct component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
 import type {Component} from 'solid-js';
-
+import Index from "./Header";
 import logo from './logo.svg';
 import styles from './App.module.scss';
 
 const App: Component = () => {
     return (
         <div class={styles.App}>
-            <header class={styles.header}>
-                <div class={styles.logoContainer}>
-                    <a href="/">
-                        <img src="/logo.png" alt="DevQuest"/>
-                    </a>
-                </div>
 
-              <div class={styles.navBar}>
-                <ul>
-                    <div class={styles.navLink}>
-                        <a href="/">
-                            Accueil
-                        </a>
-                    </div>
-
-                    <div class={styles.navLink}>
-                        <a href="/team">
-                            L'équipe
-                        </a>
-                    </div>
-                </ul>
-              </div>
-            </header>
-
+            <Index />
 
           <div>
 
