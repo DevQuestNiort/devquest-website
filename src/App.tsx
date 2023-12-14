454eb3c917cd955c70abf2a553145a5eaca9c5bc
Alex <alex.pacaud.79@gmail.com>
2023-12-14 22:45:37 +0100

    :lipstick: New UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,113 @@
 import type {Component} from 'solid-js';
-import logo from './logo.svg';
 import styles from './App.module.scss';
 
 const App: Component = () => {
     return (
         <div class={styles.App}>
             <div id={styles.main}>
+                <div id={styles.brand}>
+                    <img id={styles.logo} src="LOGO.svg" alt="DevQuest, le 14 Juin 2024 à Niort"/>
 
-                <img id={styles.logo} src="logo-date-lieu.png" alt="DevQuest, le 14 Juin 2024 à Niort"/>
+                    <h1>Le premier rassemblement dev niortais</h1>
 
+                    <h2>Tu veux être speaker ?</h2>
 
-                <div class={styles.row}>
-                    <div>
+                    <div class={styles.row}>
+                        <div>
 
-                        <a class={styles.btn} href="https://conference-hall.io/public/event/geINICiIQFU0WdORU423">DEVENIR SPEAKER</a>
+                            <a class={styles.btn} href="https://conference-hall.io/public/event/geINICiIQFU0WdORU423">Proposer
+                                un sujet</a>
+                        </div>
 
-                        <a class={styles.btn} href="">BILLETTERIE (Bientôt dispo)</a>
                     </div>
-
                 </div>
 
                 <div id={styles.infos}>
+                    <h1>Des conférences. Des sangliers. De la cervoise.</h1>
 
-                    <div>
-
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d350555.52676938265!2d-1.7364552520404568!3d46.65214213844539!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48072fcbc6eabbfb%3A0x4febad6b6c995284!2sParc%20des%20Expositions%20de%20Noron!5e0!3m2!1sen!2sfr!4v1702323019379!5m2!1sen!2sfr"
-                            width="600" height="450" style="border:0;" loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"></iframe>
+                    <div class={styles.card}>
+                        L'été prochain, ne manquez pas le premier événement ludico-professionnel niortais dédié aux
+                        développeurs. Améliorez vos compétences et surtout... Profitez de l'aventure !
                     </div>
 
-                    <div>
+                    <div id={styles.rowrs}>
+                        <a class={styles.rs}
+                           href="https://conference-hall.io/public/event/geINICiIQFU0WdORU423">
+                            <img height="32" width="32" src="logo-linkedin.png" alt="LinkedIn"/>
+                        </a>
 
-                        <p>L'évènement se veut communautaire et l'équipe n'est pas rémunérée pour l'organisation de celle ci.
-
-                            L’objectif du DevQuest, association loi 1901 créée en 2023, est de promouvoir les différents secteurs de la tech au sein du bassin Niortais au travers d'événements divers.
-
-                            La première instance du festival DevQuest aura lieu en Juin 2024 au parc Expo Noron de Niort.
-
-                            Voici l'équipe à l'oeuvre pour cette première saison.</p>
-
-                        <h3>NOUS CONTACTER</h3>
-
-                        EMAIL : <a href="mailto:bureau@devquest.fr">bureau@devquest.fr</a>
+                        <a class={styles.rs}
+                           href="https://conference-hall.io/public/event/geINICiIQFU0WdORU423">
+                            <img height="32" width="32" src="logo-x.png" alt="LinkedIn"/>
+                        </a>
                     </div>
+                </div>
+
+                <div id={styles.equipe}>
+                    <h1>L'équipe</h1>
+                    <div id={styles.galery}>
+                        <div class={styles.cardSquad}>
+                            <img class={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+
+                        <div className={styles.cardSquad}>
+                            <img className={styles.avatar} src="https://i.pravatar.cc/128" alt="John"/>
+                            <h2>John</h2>
+                            <span>#Touriste</span>
+                        </div>
+                    </div>
+
+                    <p>2024 - DevQuest - <a href="#">Mentions légales</a></p>
                 </div>
 
             </div>
