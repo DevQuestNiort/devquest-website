11ac09745f76685a1d4773a6a028dd18cdc27fb5
Samuel NAIT <nait.samuel@gmail.com>
2023-12-21 23:09:19 +0100

    :sparkles: Ajouts et corrections en vrac - Suppression de la date dans le alt du logo - Refonte de l'affichage des avatars en mode aléatoire - Corrections d'accessibilité sur les liens sociaux - Ajout d'un lien "mailto" aux liens sociaux - Ajout des GitHub et Linkedin sur les membres - :package: Build d'un nouveau livrable

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,15 +1,24 @@
-import styles from "./Avatar.module.scss";
+import style from "./Avatar.module.scss";
 
 interface AvartarProperties {
-    img: string
-    name: string
-    info: string
+    readonly img: string
+    readonly name: string
+    readonly role: string
+    readonly github?: string;
+    readonly linkedin?: string;
+
 }
 
-export default function Avatar({img, name, info}: AvartarProperties) {
+export default function Avatar({ img, name, role, github, linkedin }: AvartarProperties) {
     return <div>
-        <img class={styles.avatar} height="128" width="128" src={img} alt={name}/>
+        <img class={style.avatar} height="128" width="128" src={img} alt={name} />
         <h2>{name}</h2>
-        <span>{info}</span>
+        <div class={style.infos}>
+
+            <p>{role}</p>
+            {github && <a target="_blank" href={`https://github.com/${github}`}><span class="icon-github"></span></a>}
+            {linkedin && <a target="_blank" href={`https://www.linkedin.com/in/${linkedin}`}><span class="icon-linkedin"></span></a>}
+        </div>
+
     </div>
 }
