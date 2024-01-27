db069995a523962efbe56cb06ba1f56cf21279c6
Samuel NAIT <nait.samuel@gmail.com>
2024-01-27 22:02:45 +0100

    :sparkles: Add the Date & few tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -93,26 +93,31 @@ const App: Component = () => {
       <div id={styles.main}>
         <div id={styles.brand}>
           <img
-            id={styles.logo}
-            src="logo.svg"
-            alt="DevQuest"
+              id={styles.logo}
+              src="logo.svg"
+              alt="DevQuest"
           />
 
           <h1>Le premier rassemblement dev niortais</h1>
+          <h2>Le 14 Juin 2024 au <a
+              href="https://www.google.com/maps/place/Parc+des+Expositions+de+Noron/@46.3294067,-0.4903639,17z/data=!3m1!4b1!4m6!3m5!1s0x48072fcbc6eabbfb:0x4febad6b6c995284!8m2!3d46.3294067!4d-0.4903639!16s%2Fg%2F1tq6jg6w?entry=ttu">
+            Parc des Expositions de Noron
+          </a>
+          </h2>
 
           <div class={styles.row}>
             <div>
               <a
-                class={styles.btn}
-                href="https://conference-hall.io/public/event/geINICiIQFU0WdORU423"
+                  class={styles.btn}
+                  href="https://conference-hall.io/public/event/geINICiIQFU0WdORU423"
               >
                 Proposer un sujet
               </a>
             </div>
             <div>
               <a
-                class={styles.btn}
-                href="https://www.billetweb.fr/devquest-niort-2024"
+                  class={styles.btn}
+                  href="https://www.billetweb.fr/devquest-niort-2024"
               >
                 Accéder à la billeterie
               </a>
@@ -129,26 +134,26 @@ const App: Component = () => {
             compétences et surtout... Profitez de l'aventure !
           </div>
 
-          <div id={styles.rowrs}>
+          <div id={styles.rows}>
             <a
-              class={styles.rs}
-              href="https://www.linkedin.com/company/devquest-niort/"
-              aria-label="Linkedin DevQuest"
+                class={styles.rs}
+                href="https://www.linkedin.com/company/devquest-niort/"
+                aria-label="Linkedin DevQuest"
             >
               <img
-                height="32"
-                width="32"
-                src="/social/logo-linkedin.png"
-                aria-hidden="true"
+                  height="32"
+                  width="32"
+                  src="/social/logo-linkedin.png"
+                  aria-hidden="true"
               />
             </a>
 
             <a class={styles.rs} href="https://twitter.com/DevQuestNiort" aria-label="Twitter DevQuest">
-              <img height="32" width="32" src="/social/logo-x.png" aria-hidden="true" />
+              <img height="32" width="32" src="/social/logo-x.png" aria-hidden="true"/>
             </a>
 
             <a class={styles.rs} href="mailto:bureau@devquest.fr" aria-label="Envoyer un mail à bureau@devquest.fr">
-              <span class="icon-mail" />
+              <span class="icon-mail"/>
             </a>
           </div>
         </div>
@@ -156,22 +161,22 @@ const App: Component = () => {
           <h1>Diantre, DevQuest dîtes-vous? Qu'est-ce donc que cela?</h1>
           <div id={styles.eventspec}>
             <div>
-              <img height="96" src="/assets/backpack.png" aria-hidden="true" />
+              <img height="96" src="/assets/backpack.png" aria-hidden="true"/>
               {/* <a href="https://www.flaticon.com/free-icons/rpg" title="rpg icons">Rpg icons created by Freepik - Flaticon</a> */}
               <h2>1 journée</h2>
             </div>
             <div>
-              <img height="96" src="/assets/knight.png" aria-hidden="true" />
+              <img height="96" src="/assets/knight.png" aria-hidden="true"/>
               {/* <a href="https://www.flaticon.com/free-icons/rpg" title="rpg icons">Rpg icons created by Freepik - Flaticon</a> */}
               <h2>150 spectateurs</h2>
             </div>
             <div>
-              <img height="96" src="/assets/king.png" aria-hidden="true" />
+              <img height="96" src="/assets/king.png" aria-hidden="true"/>
               {/* <a href="https://www.flaticon.com/free-icons/king" title="king icons">King icons created by Freepik - Flaticon</a> */}
               <h2>16 partenaires</h2>
             </div>
             <div>
-              <img height="96" src="/assets/quest.png" aria-hidden="true" />
+              <img height="96" src="/assets/quest.png" aria-hidden="true"/>
               {/* <a href="https://www.flaticon.com/free-icons/rpg" title="rpg icons">Rpg icons created by David Carapinha - Flaticon</a> */}
               <h2>20 présentations multi format</h2>
             </div>
@@ -182,13 +187,13 @@ const App: Component = () => {
           <h1>L'équipe</h1>
           <div id={styles.galery}>
             {members.sort(() => (Math.random() > .5) ? 1 : -1).map((m: Member) =>
-              <Avatar
-                img={m.img}
-                name={m.name}
-                role={m.role}
-                github={m.github}
-                linkedin={m.linkedin}
-              />
+                <Avatar
+                    img={m.img}
+                    name={m.name}
+                    role={m.role}
+                    github={m.github}
+                    linkedin={m.linkedin}
+                />
             )}
 
           </div>
@@ -196,10 +201,19 @@ const App: Component = () => {
 
         <div id={styles.program}>
           <h1>Le programme</h1>
-          <p id={styles.programtext} >Le programme n'est pas encore disponible, mais d'ores et déjà, nous pouvons vous dire que les histoires que vous entendrez vous raconterons des épopées sur l'intelligence artificielle mais aussi autour de l'artisanat logiciel... ou bien encore d'autres sujets passionnants!</p>
-          <p>2024 - DevQuest</p>
+          <div class={styles.card}>
+
+
+            <p id={styles.programtext}>Le programme n'est pas encore disponible, mais d'ores et déjà, nous pouvons vous
+              dire que les histoires que vous entendrez vous raconterons des épopées sur l'intelligence artificielle
+              mais aussi autour de l'artisanat logiciel... ou bien encore d'autres sujets passionnants!</p>
+          </div>
+
+          <p aria-hidden="true"><b>2024 - DevQuest</b></p>
+
         </div>
       </div>
+
     </div>
   );
 };
