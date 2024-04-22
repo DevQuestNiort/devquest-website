0aa014c1aedc0fcb2d22e30c91ac09aa5e3030f9
Alex <alex.pacaud.79@gmail.com>
2024-04-22 22:16:07 +0200

    feat: add link code of conduct

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,9 @@ import Link from "next/link";
 import styles from './Footer.module.scss'
 
 export const Footer = () => <footer className={styles.footer}>
-<b aria-hidden="true">@ 2024 DevQuest</b>
-{" - "}
-<Link href={"/mentions-legales"}>Mentions légales</Link>
+    <b aria-hidden="true">@ 2024 DevQuest</b>
+    {" - "}
+    <Link href={"/mentions-legales"}>Mentions légales</Link>
+    {" - "}
+    <Link href={"/code-de-conduite"}>Code de conduite</Link>
 </footer>;
