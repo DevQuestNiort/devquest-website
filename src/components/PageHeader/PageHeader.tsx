caaa74b8ad6ca034d3b20eae2d2838182d3bc6f0
vincent <vincent.gouadon@gmail.com>
2024-04-23 20:38:18 +0200

    Rework menu (responsive )

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,51 +1,129 @@
 "use client";
 import Image from "next/image";
 import styles from "./PageHeader.module.scss";
-import config from '../../data/config.json'
+import config from "../../data/config.json";
 import Link from "next/link";
 import { LinkButton } from "../LinkButton";
-import {usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+type MenuProps = {
+  pathname: string;
+};
 
 export const PageHeader = () => {
-    const pathname = usePathname ();
+  const pathname = usePathname();
 
-    const isActive = (href: string) => pathname === href;
+  return (
+    <>
+      <VersionLarge pathname={pathname} />
+      <VersionMobile pathname={pathname} />
+    </>
+  );
+};
 
-    return <nav className={styles.header}>
-        <div className={styles.containerFluid}>
-            <Link href="/">
-                <Image
-                    src={`/devquest-color.svg`}
-                    alt={"logo devquest"}
-                    width={64}
-                    height={64}
-                />
-            </Link>
+const VersionLarge = ({ pathname }: MenuProps) => {
+  const isActive = (href: string) => pathname === href;
+  return (
+    <nav className={classNames(styles.header, styles.menuLarge)}>
+      <div className={styles.containerFluid}>
+        <Link href="/">
+          <Image
+            src={`/devquest-color.svg`}
+            alt={"logo devquest"}
+            width={64}
+            height={64}
+          />
+        </Link>
 
-            <Link href="/" className={styles.otherHeaderBrand} style={{fontWeight: isActive('/') ? 'bold' : 'normal'}}>Accueil</Link>
+        <Link
+          href="/"
+          className={styles.otherHeaderBrand}
+          style={{ fontWeight: isActive("/") ? "bold" : "normal" }}
+        >
+          Accueil
+        </Link>
 
-            <span className={styles.separator}>|</span>
+        <span className={styles.separator}>|</span>
 
-            <Link href="/schedule" className={styles.otherHeaderBrand} style={{fontWeight: isActive('/schedule') || pathname.includes('/sessions') ? 'bold' : 'normal'}}>Programme</Link>
+        <Link
+          href="/schedule"
+          className={styles.otherHeaderBrand}
+          style={{
+            fontWeight:
+              isActive("/schedule") || pathname.includes("/sessions")
+                ? "bold"
+                : "normal",
+          }}
+        >
+          Programme
+        </Link>
 
-            <span className={styles.separator}>|</span>
+        <span className={styles.separator}>|</span>
 
-            <Link href={config.kitPartenaires} className={styles.otherHeaderBrand}>Kit partenaires</Link>
+        <Link href={config.kitPartenaires} className={styles.otherHeaderBrand}>
+          Kit partenaires
+        </Link>
 
-            <span className={styles.separator}>|</span>
+        <span className={styles.separator}>|</span>
 
+        <Link href={config.kitMedia} className={styles.otherHeaderBrand}>
+          Kit Média
+        </Link>
 
-            <Link href={config.kitMedia} className={styles.otherHeaderBrand}>Kit Média</Link>
-
-            <div className={styles.mainBtnContainer}>
-                {!isActive('/') && <LinkButton
-                theme="Primary"
-                classes={styles.reserverMaPlace}
-                href={config.shop}
+        <div className={styles.mainBtnContainer}>
+          {!isActive("/") && (
+            <LinkButton
+              theme="Primary"
+              classes={styles.reserverMaPlace}
+              href={config.shop}
             >
-                Réserver mes places
-            </LinkButton>}
-            </div>
+              Réserver mes places
+            </LinkButton>
+          )}
         </div>
+      </div>
     </nav>
-}
+  );
+};
+
+const VersionMobile = ({ pathname }: MenuProps) => {
+  const isActive = (href: string) => pathname === href;
+  return (
+    <nav className={classNames(styles.header, styles.menuMobile)}>
+      <div className={styles.containerFluid}>
+        <Link href="/">
+          <Image
+            src={`/devquest-color.svg`}
+            alt={"logo devquest"}
+            width={32}
+            height={32}
+          />
+        </Link>
+
+        <Link
+          href="/"
+          className={styles.otherHeaderBrand}
+          style={{ fontWeight: isActive("/") ? "bold" : "normal" }}
+        >
+          Accueil
+        </Link>
+
+        <span className={styles.separator}>|</span>
+
+        <Link
+          href="/schedule"
+          className={styles.otherHeaderBrand}
+          style={{
+            fontWeight:
+              isActive("/schedule") || pathname.includes("/sessions")
+                ? "bold"
+                : "normal",
+          }}
+        >
+          Programme
+        </Link>
+      </div>
+    </nav>
+  );
+};
