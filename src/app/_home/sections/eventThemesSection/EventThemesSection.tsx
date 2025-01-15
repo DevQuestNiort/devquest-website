676d2647dd1e37d1d745afb176f66d936e6117bc
Alexandre Guérin <41384355+Ithrandil@users.noreply.github.com>
2025-01-15 13:37:33 +0100

    refactor: added section components to home page (#103)

---
+++ src/app/_home/sections/eventThemesSection/EventThemesSection.tsx
@@ -0,0 +1,27 @@
+import { Section } from "@/components/Section";
+import { Theme } from "@/model/Theme";
+import { Thematique } from "@/components/Thematique";
+
+import styles from "./EventThemesSection.module.scss";
+
+interface EventThemesSectionProperties {
+  readonly themes: Theme[];
+}
+
+export function EventThemesSection({themes}: EventThemesSectionProperties) {
+    return (
+        <Section theme="Dark">
+        <h2>Les classes disponibles</h2>
+        <div className={styles.themeContainer}>
+          {themes.map((theme, i) => (
+            <Thematique
+              key={i}
+              img={theme.img}
+              name={theme.name}
+              details={theme.details}
+            />
+          ))}
+        </div>
+      </Section>
+    );
+}
