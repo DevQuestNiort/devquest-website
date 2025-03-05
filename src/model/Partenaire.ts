387e34bd15ec8703863db955e14087ac230b0af2
Ithrandil <alexandre.guerin17@gmail.com>
2025-03-06 00:26:43 +0100

    feat: added 2025 partners 🍱💄

--- src/model/Partenaire.ts
+++ src/model/Partenaire.ts
@@ -2,5 +2,7 @@ export interface Partenaire {
   name: string;
   asset: string;
   site?: string;
+  actif: boolean;
+  consolidated: boolean;
   level:  'LEGENDAIRE' | 'EPIQUE'| 'RARE' | 'COMMUN' | 'AUTRE' ;
 }
