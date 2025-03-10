635c39a2e48a58d7ae22434152f3cab4bda0b769
Ithrandil <alexandre.guerin17@gmail.com>
2025-03-10 08:41:05 +0100

    feat: added Macif and removed consolidated value

--- src/model/Partenaire.ts
+++ src/model/Partenaire.ts
@@ -2,7 +2,5 @@ export interface Partenaire {
   name: string;
   asset: string;
   site?: string;
-  actif: boolean;
-  consolidated: boolean;
   level:  'LEGENDAIRE' | 'EPIQUE'| 'RARE' | 'COMMUN' | 'AUTRE' ;
 }
