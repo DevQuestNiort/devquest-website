13a7d9acc10d15396efd5ff6ff44be666a61d238
Florent FREMONT <ff.fremont.florent@gmail.com>
2025-03-31 11:15:01 +0200

    fix: build programmation

--- src/app/speaker/[slug]/page.tsx
+++ src/app/speaker/[slug]/page.tsx
@@ -6,65 +6,70 @@ import { Avatar } from "@/components/Avatar";
 import { Card } from "@/components/Card";
 import Link from "next/link";
 
-
 const getSpeakers = async () =>
-    JSON.parse(
-        await fs.readFile(process.cwd() + "/src/data/speakers.json", "utf8"),
-    ) as SpeakerModel[];
+  JSON.parse(
+    await fs.readFile(process.cwd() + "/src/data/speakers.json", "utf8"),
+  ) as SpeakerModel[];
 
 const getSessions = async () =>
-    JSON.parse(
-        await fs.readFile(process.cwd() + "/src/data/sessions.json", "utf8"),
-    ) as Session[];
+  JSON.parse(
+    await fs.readFile(process.cwd() + "/src/data/sessions.json", "utf8"),
+  ) as Session[];
 
 export async function generateStaticParams() {
-    const speakers = await getSpeakers();
+  const speakers = await getSpeakers();
 
-    return speakers.map((speaker) => ({
-        slug: speaker.id
-    }));
+  return speakers.map((speaker) => ({
+    slug: speaker.id,
+  }));
 }
 
-const Speaker = async ({
-    params,
-}: {
-    params: Promise<{ slug: string }>
-}) => {
-    const speakerId = (await params).slug;
-    const { sessionsId, ...speaker } = (await getSpeakers()).find(s => s.id === speakerId) as SpeakerModel;
-    const allSession = await getSessions();
-    const speakerSessions: Session[] = [];
-    sessionsId.forEach(sessionId => {
-        speakerSessions.push(allSession.find(session => session.id === sessionId) as Session);
-    });
+const Speaker = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const speakerId = (await params).slug;
+  const { sessionsId, ...speaker } = (await getSpeakers()).find(
+    (s) => s.id === speakerId,
+  ) as SpeakerModel;
+  const allSession = await getSessions();
+  const speakerSessions: Session[] = [];
+  sessionsId.forEach((sessionId) => {
+    speakerSessions.push(
+      allSession.find((session) => session.id === sessionId) as Session,
+    );
+  });
 
-    return (
+  return (
     <div className={styles.speakerContainer}>
-        <Avatar
-            img={speaker.picture || "/icons-rp/role-playing.png"}
-            name={speaker.name}
-            company={speaker?.company}
-            github={speaker.social?.github}
-            linkedin={speaker.social?.linkedin}
-            x={speaker.social?.x}
-        />
-        {speaker.bio && <Card className={styles.bioCardSpeaker}>{speaker.bio}</Card>}
-        {speakerSessions.map((session) => (
-                <Link href={`/sessions/${session.slug}`} className={styles.disableLinkStyle}>
-                    <Card key={session.id} className={styles.confCardSpeaker}>
-                        <p>{session.title}</p>
-                        <div className={styles.sessionInformations}>
-                            {/*TODO: intégrer les mini cards faites dans le descriptif des conf pour la salle, jour et heure,
+      <Avatar
+        img={speaker.picture || "/icons-rp/role-playing.png"}
+        name={speaker.name}
+        company={speaker?.company}
+        github={speaker.social?.github}
+        linkedin={speaker.social?.linkedin}
+        x={speaker.social?.x}
+      />
+      {speaker.bio && (
+        <Card className={styles.bioCardSpeaker}>{speaker.bio}</Card>
+      )}
+      {speakerSessions.map((session) => (
+        <Link
+          key={session.id}
+          href={`/sessions/${session.slug}`}
+          className={styles.disableLinkStyle}
+        >
+          <Card className={styles.confCardSpeaker}>
+            <p>{session.title}</p>
+            <div className={styles.sessionInformations}>
+              {/*TODO: intégrer les mini cards faites dans le descriptif des conf pour la salle, jour et heure,
                             tout sur une ligne */}
-                            <div>{session.categorie}</div>
-                            <div>JOUR</div>
-                            <div>{session.room}</div>
-                        </div>
-                    </Card>
-                </Link>            
-))}
+              <div>{session.categorie}</div>
+              <div>JOUR</div>
+              <div>{session.room}</div>
+            </div>
+          </Card>
+        </Link>
+      ))}
     </div>
-    )
-}
+  );
+};
 
 export default Speaker;
