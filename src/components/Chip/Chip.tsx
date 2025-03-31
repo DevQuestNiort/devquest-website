55fdbd2fa90d78ee8f148b230b05ea1d789a1890
Florent FREMONT <ff.fremont.florent@gmail.com>
2025-03-31 10:59:36 +0200

    feat: day 2 programmation

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,47 +1,57 @@
 import { ReactElement } from "react";
-import styles from './Chip.module.scss'
+import styles from "./Chip.module.scss";
 import classNames from "classnames";
 
-
-
 export const tagLabels = {
-
-    backend: {
-        label: "Backend",
-        icon: <span className="material-symbols-outlined">code</span>,
-    },
-    discovery: {
-        label: "Quêtes secondaires",
-        icon: <span className="material-symbols-outlined">lightbulb</span>,
-    },
-    cloud_devops: {
-        label: "Cloud & DevSecOps",
-        icon: <span className="material-symbols-outlined">cloud</span>,
-    },
-    architecture: {
-        label: "Méthode & Architecture",
-        icon: <span className="material-symbols-outlined">architecture</span>,
-    },
-    tremplin: {
-        label: "Tremplin",
-        icon: <span className="material-symbols-outlined">celebration</span>,
-    },
-    frontend: {
-        label: "Frontend",
-        icon: <span className="material-symbols-outlined">web</span>,
-    },
-    ux_ui: {
-        label: "UX / UI",
-        icon: <span className="material-symbols-outlined">brush</span>,
-    },
-    keynote: {
-        label: "Keynote",
-        icon: <span className="material-symbols-outlined">celebration</span>,
-    },
+  backend: {
+    label: "Backend",
+    icon: <span className="material-symbols-outlined">code</span>,
+  },
+  discovery: {
+    label: "Quêtes secondaires",
+    icon: <span className="material-symbols-outlined">lightbulb</span>,
+  },
+  cloud_devops: {
+    label: "Cloud & DevSecOps",
+    icon: <span className="material-symbols-outlined">cloud</span>,
+  },
+  ia: {
+    label: "IA",
+    icon: <span className="material-symbols-outlined">robot_2</span>,
+  },
+  architecture: {
+    label: "Méthode & Architecture",
+    icon: <span className="material-symbols-outlined">architecture</span>,
+  },
+  tremplin: {
+    label: "Tremplin",
+    icon: <span className="material-symbols-outlined">celebration</span>,
+  },
+  frontend: {
+    label: "Frontend",
+    icon: <span className="material-symbols-outlined">web</span>,
+  },
+  ux_ui: {
+    label: "UX / UI",
+    icon: <span className="material-symbols-outlined">brush</span>,
+  },
+  keynote: {
+    label: "Keynote",
+    icon: <span className="material-symbols-outlined">celebration</span>,
+  },
 };
 
-
-export const Chip = ({ icon, label, classes }: { icon: ReactElement, label: string, classes?: string }) => <span className={classNames(styles.chip, classes)}>
+export const Chip = ({
+  icon,
+  label,
+  classes,
+}: {
+  icon: ReactElement;
+  label: string;
+  classes?: string;
+}) => (
+  <span className={classNames(styles.chip, classes)}>
     {icon}
     <span className={styles.chipLabel}>{label}</span>
-</span>
+  </span>
+);
--- src/model/Session.ts
+++ src/model/Session.ts
@@ -1,17 +1,25 @@
-export interface Session  {
-    id: string,
-    slug: string,
-    title: string,
-    format: Format,
-    categorie: string,
-    abstract: string,
-    room: string,
-    speakersId: string[],
-    tags: Tags[],
-    cancelled: boolean,
-    slot: string
+export interface Session {
+  id: string;
+  slug: string;
+  title: string;
+  format: Format;
+  categorie: string;
+  abstract: string;
+  room: string;
+  speakersId: string[];
+  tags: Tags[];
+  cancelled: boolean;
+  slot: string;
 }
 
 export type Format = "Workshop" | "Quickie" | "Workshop";
 
-export type Tags = 'backend' | 'frontend' | 'discovery' | 'cloud_devops' | 'tremplin' | 'architecture' | 'ux_ui';
+export type Tags =
+  | "backend"
+  | "frontend"
+  | "discovery"
+  | "cloud_devops"
+  | "ia"
+  | "tremplin"
+  | "architecture"
+  | "ux_ui";
