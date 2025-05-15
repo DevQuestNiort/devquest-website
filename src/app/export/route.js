5faa7af67f2138bce51ac1d0924fd63e76f73dd8
vincent <vincent.gouadon@gmail.com>
2025-05-15 11:18:29 +0200

    update Export openFeedback

--- src/app/export/route.js
+++ src/app/export/route.js
@@ -1,30 +1,30 @@
 import { promises as fs } from "fs";
+
 const getSessions = async () =>
   JSON.parse(
     await fs.readFile(process.cwd() + "/src/data/sessions.json", "utf8"),
   );
 
 const getSlots = async () =>
+  JSON.parse(await fs.readFile(process.cwd() + "/src/data/slots.json", "utf8"));
+
+const getSpeakers = async () =>
   JSON.parse(
-    await fs.readFile(process.cwd() + "/src/data/slots.json", "utf8"),
+    await fs.readFile(process.cwd() + "/src/data/speakers.json", "utf8"),
   );
 
-const getSpeakers = async () => JSON.parse(await fs.readFile(
-  process.cwd() + "/src/data/speakers.json",
-  "utf8",
-));
-
 export async function GET() {
-  const speakers = (await getSpeakers());
-  const slots = (await getSlots())
+  const speakers = await getSpeakers();
+  const slots = await getSlots();
   const sessions = (await getSessions())
     .map((session) => ({
       ...session,
       slot: slots.find((s) => s.key === session.slot),
-      speakers: session.speakersId.map(speakerId => speakers.find((s) => s.id === speakerId))
+      speakers: session.speakersId.map((speakerId) =>
+        speakers.find((s) => s.id === speakerId),
+      ),
     }))
     .filter((session) => session.slot);
-
 
   const datasession = sessions.reduce((agr, session) => {
     const worksession = {
@@ -33,27 +33,26 @@ export async function GET() {
       tags: session.tags,
       title: session.title,
 
-      trackTitle: session.room
-    }
-    agr[session.id] = worksession
-    return agr
-  }, {})
+      trackTitle: session.room,
+    };
+    agr[session.id] = worksession;
+    return agr;
+  }, {});
 
   const dataSpeaker = speakers.reduce((agr, speaker) => {
-    let name = speaker.name
+    let name = speaker.name;
     if (name === "") {
-      name = "void"
+      name = "surprise";
     }
     const workSpeak = {
       id: speaker.id,
       name: name,
-      photoUrl: "https://www.devquest.fr" + speaker.picture
-    }
-    agr[speaker.id] = workSpeak
-    return agr
-  },
-    {})
-  const data = { sessions: datasession, speakers: dataSpeaker }
+      photoUrl: "https://www.devquest.fr" + speaker.picture,
+    };
+    agr[speaker.id] = workSpeak;
+    return agr;
+  }, {});
+  const data = { sessions: datasession, speakers: dataSpeaker };
 
-  return Response.json( data )
+  return Response.json(data);
 }
