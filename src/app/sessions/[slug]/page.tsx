050e45cfee6bfed6e84d17499faa31ba34c503d5
Vincent Gouadon <vincent.gouadon@live.fr>
2026-04-30 11:09:35 +0200

    urgent push

--- src/app/sessions/[slug]/page.tsx
+++ src/app/sessions/[slug]/page.tsx
@@ -66,7 +66,7 @@ const Session = async ({ params: { slug } }: SessionProps) => {
   }
   const myFullSession = session as FullSession;
 
-  const DISPLAY_OPENFEEDBACK = true;
+  const DISPLAY_OPENFEEDBACK = false;
 
   return (
     <div className={styles.container}>
